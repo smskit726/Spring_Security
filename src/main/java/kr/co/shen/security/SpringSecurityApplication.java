0b644cae7494b9e0208d082354130ebf5e41a3dc
= <=>
2024-08-12 15:56:01 +0900

    caffeine cache 의존성 추가, 설정

--- src/main/java/kr/co/shen/security/SpringSecurityApplication.java
+++ src/main/java/kr/co/shen/security/SpringSecurityApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-// 캐시 사용을 위해 @EnableCaching 추가
 @SpringBootApplication
-@EnableCaching
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
